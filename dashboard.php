4e4150c47f9d8a27e02412db5918b5e23b4f67f7
Razvan Bimbasa <razvan@Razvans-MacBook-Pro.local>
2025-01-14 01:26:54 +0200

    Added bootstrap, and started work on admin dashboard

---
+++ dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Admin Dashboard</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        
+    </head>
+    <body>
+        <h1>Test</h1>
+    </body>
+</html>
