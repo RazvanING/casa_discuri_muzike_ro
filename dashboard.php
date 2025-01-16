81c06370421a376db6b5a99d273fc85964302f3f
Razvan Bimbasa <razvan@Razvans-MacBook-Pro.local>
2025-01-16 16:52:57 +0200

    Work progressing on admin dashboard

--- dashboard.php
+++ dashboard.php
@@ -5,9 +5,48 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/adminstyles.css">
         
     </head>
     <body>
-        <h1>Test</h1>
+        <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2">
+                <h1>Razvan</h1>
+                <ul id="Side_menu" class="nav nav-pills nav-stacked">
+                    
+                <li class="active"> <a href="dashboard.php">Dashboard</a> </li>
+                <li>  <a href="#">Add new post</a></li>
+                <li>    <a href="#">Categories</a></li>
+                <li>    <a href="#">Manage admins</a></li>
+                <li>    <a href="#">Comments</a></li>
+                <li>    <a href="#">Live blog</a></li>
+                <li>    <a href="#">Logout</a></li>
+                   
+                </ul>
+            </div> <!--Aici se termina div-ul pentru coloana lateral stanga -->
+                <div class="col-sm-10">
+                    <h4> about us</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit labore accusantium deserunt dolore est animi nesciunt, voluptatibus at maiores, laudantium pariatur impedit perferendis aliquam libero iusto iure quas et aliquid!</p>
+                    <h4> about us</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit labore accusantium deserunt dolore est animi nesciunt, voluptatibus at maiores, laudantium pariatur impedit perferendis aliquam libero iusto iure quas et aliquid!</p>
+                    <h4> about us</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit labore accusantium deserunt dolore est animi nesciunt, voluptatibus at maiores, laudantium pariatur impedit perferendis aliquam libero iusto iure quas et aliquid!</p>
+                    <h4> about us</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit labore accusantium deserunt dolore est animi nesciunt, voluptatibus at maiores, laudantium pariatur impedit perferendis aliquam libero iusto iure quas et aliquid!</p>
+                    <h4> about us</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit labore accusantium deserunt dolore est animi nesciunt, voluptatibus at maiores, laudantium pariatur impedit perferendis aliquam libero iusto iure quas et aliquid!</p>
+                    <h4> about us</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit labore accusantium deserunt dolore est animi nesciunt, voluptatibus at maiores, laudantium pariatur impedit perferendis aliquam libero iusto iure quas et aliquid!</p>
+                    <h4> about us</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit labore accusantium deserunt dolore est animi nesciunt, voluptatibus at maiores, laudantium pariatur impedit perferendis aliquam libero iusto iure quas et aliquid!</p>
+                    <h4> about us</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit labore accusantium deserunt dolore est animi nesciunt, voluptatibus at maiores, laudantium pariatur impedit perferendis aliquam libero iusto iure quas et aliquid!</p>
+
+                </div> <!--Aici se termina div-ul pentru zona principala de continut -->
+
+            </div> <!--Aici se termina div-ul pentru coloane -->
+        </div><!--Aici se termina div-ul pentru container-->
+ 
     </body>
 </html>
